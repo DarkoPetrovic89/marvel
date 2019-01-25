6e29ec3f753554db03eb27fef42d2e77df5c16c9
Darko Petrovic <darko.petrovic@quantox.com>
2019-01-25 17:41:48 +0100

    petak 25.01

--- main2.js
+++ main2.js
@@ -17,6 +17,10 @@ var arrayFromLS = [];
 var arrayItemFromLS;
 var starId;
 var numbers = [];
+var searchArr = [];
+var x;
+var itemsPerPage = 12;
+var searchInput;
 
 // Setup our listener to process completed requests
 xhr.onload = function () {
@@ -65,16 +69,25 @@ search.addEventListener("keyup", function () {
 //Function for filter hero
 function filterHero() {
     checkBookmarkStar();
-    var searchInput = document.getElementById('search').value.toUpperCase().replace(/\s/g, "");
+    searchInput = document.getElementById('search').value.toUpperCase().replace(/\s/g, "");
     for (var i = 0; i < itemsArr.length; i++){
         var a = itemsArr[i];
         img_holder = document.querySelectorAll('.imgHolder');
         if (a.toUpperCase().indexOf(searchInput) > -1){
             img_holder[i].style.display = 'inline-block';
+            img_holder[i].classList.add('checked');
+            if (i > itemsPerPage){
+                img_holder[i].classList.add('rest');
+            }
         }else {
             img_holder[i].style.display = 'none';
+            img_holder[i].classList.remove('checked');
         }
     }
+    searchResult();
+    paginateArr();
+    showMore();
+    // onChange();
     for ( let w=0; w<itemsArr.length; w++){
         star[w].addEventListener('click' , function () {
             starId = star[w].id;
@@ -149,6 +162,50 @@ document.getElementById('clearHistory').addEventListener('click' , function () {
 });
 
 
+function searchResult() {
+    searchArr = [];
+    for(var c=0; c<document.querySelectorAll('.checked').length; c++){
+        searchArr.push(document.querySelectorAll('.checked')[c])
+    }
+}
+function paginateArr() {
+    x = paginate(searchArr, 12 , 1);
 
+    function paginate (array, page_size, page_number) {
+        --page_number; // because pages logically start with 1, but technically with 0
+        return array.slice(page_number * page_size, (page_number + 1) * page_size);
+    }
+
+}
+
+function showMore() {
+    var btn = document.createElement("button");
+    btn.classList.add('showMore');
+    var text = document.createTextNode('SHOW MORE');
+    if (searchArr.length > itemsPerPage){
+        btn.appendChild(text);
+        document.getElementById('itemHolder').appendChild(btn);
+        this.addEventListener('click' , function () {
+            for(let i = 0; i<searchArr.length; i++){
+                img_holder[i].classList.remove('rest');
+            }
+        })
+    }
+}
+
+
+document.getElementById('search').oninput = function () {
+    if(document.querySelector('.showMore') !== null){
+        document.querySelector('.showMore').remove()
+    }
+    if(searchArr.length < itemsPerPage && document.querySelector('.showMore') !== null){
+        document.querySelector('.showMore').remove()
+    }
+
+};
+
+// document.getElementsByClassName('showMore').addEventListener('click' , function () {
+//         alert(event.target)
+// });
 
 
